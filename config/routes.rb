0d6e752178451e24fcbe736845bb5b17194a7e6e
jrkinouani <kinouani@gmail.com>
2022-03-21 12:39:01 +0100

    add page about

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   #get 'static_pages/home'
   root to: 'static_pages#home'
+    get '/about' , to: "static_pages#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
